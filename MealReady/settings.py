924cff9bc26f7a3a4d6ba5ed16a94c24e8afa475
arun-9398 <arunkumar93988064@gmail.com>
2026-08-17 21:03:51 +0530

    Configure production settings

--- MealReady/settings.py
+++ MealReady/settings.py
@@ -23,9 +23,17 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-!t$^_0#*=2hk=ut9sf*m8aozx#y50e*7fnfuvg!xtr@5@i3d5j'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'mealready.onrender.com',
+    'localhost',
+    '127.0.0.1',
+]
+
+CSRF_TRUSTED_ORIGINS = [
+    'https://mealready.onrender.com',
+]
 
 
 # Application definition
